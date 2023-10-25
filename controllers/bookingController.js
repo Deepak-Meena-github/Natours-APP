2a35c6b1fccb7bcf293d4587c9418d0b69dde3ab
Deepak Meena <meenadeepraj1@gmail.com>
2023-10-25 11:05:38 +0530

    completed_all_part

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -15,7 +15,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
-   success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+   success_url: `${req.protocol}://${req.get('host')}/?tour=${
       req.params.tourId
     }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
